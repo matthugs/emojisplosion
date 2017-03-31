d5330f5d9ed74a93af2e8ca9da8cf3e5459fa4bf
Matt Hughes <mahughes@wayfair.com>
2017-03-31 18:32:00 -0400

    Add transparent space around non-square input emoji for explode-

--- explode-emoji.sh
+++ explode-emoji.sh
@@ -6,4 +6,4 @@ input_file_name="$(basename "${input_file_name_raw}")"
 # "file.blah" -> "file-explosion.gif"
 file_name_to_output="${input_file_name%.*}-explosion.gif"
 
-convert -dispose Previous "${input_file_name_raw}" -resize 128x128 -set page +0+0 -set delay 100 explosion.gif -loop 0 "${file_name_to_output}";
+convert -dispose Previous "${input_file_name_raw}" -resize 128x128 -background transparent -gravity center -extent 128x128 -set page +0+0 -set delay 100 explosion.gif -loop 0 "${file_name_to_output}";
