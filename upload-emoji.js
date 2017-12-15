fd39f7a568fc62ab5367a4a9c1e68d4b0807a9ad
Matt Hughes <mahughes@wayfair.com>
2017-12-15 16:54:40 -0500

    Better documentation for the upload emoji script, stylistic changes

--- upload-emoji.js
+++ upload-emoji.js
@@ -1,12 +1,25 @@
+/**
+ * JS for mass-uploading emoji, to be copied-and-pasted into the
+ * browser javascript console on
+ * <$your_organization>.slack.com/customize/emoji. Tested primarily on
+ * Chrome. (Yes, jQuery is already on the page.)
+ *
+ * Once this is loaded you can drag and drop files from your OS's file
+ * explorer (e.g. Finder or Windows Explorer) onto the emoji name text
+ * input on the page. The emoji will be uploaded under the name of the
+ * file. You can select as many files as you like to drag and drop
+ * (but try not to overload slack, or they'll surely break this for
+ * everyone).
+ */
 $('form').on('drop', function(e) {
   e.preventDefault();
   e.stopPropagation();
-  var files = e.originalEvent.dataTransfer.files;
-  for(var i = 0; i < files.length; i++) {
-    var file = files[i];
-    var ajaxData = new FormData($('form').get(0));
+  const files = e.originalEvent.dataTransfer.files;
+  for(let i = 0; i < files.length; i++) {
+    const file = files[i];
+    const ajaxData = new FormData($('form').get(0));
     ajaxData.append('img', file);
-    var fileName = file.name,
+    const fileName = file.name,
         name = fileName.substr(0, fileName.lastIndexOf('.')) || fileName;
     ajaxData.append('name', name);
     $.ajax({
@@ -19,4 +32,3 @@ $('form').on('drop', function(e) {
     });
   }
 });
-
